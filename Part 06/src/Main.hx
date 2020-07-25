1e53e1c47a9a4fd452b93c63cdfe555959e013b3
tobil4sk <41230637+tobil4sk@users.noreply.github.com>
2020-07-25 12:18:30 +0100

    Part 06

---
+++ Part 06/src/Main.hx
@@ -0,0 +1,32 @@
+class Main {
+	static function main() {
+		var doorOpen:Bool = true;
+		// trace(doorOpen);
+
+		var name1 = "Bucky";
+		var name2 = "Sally";
+
+		var namesSame = name1 == name2;
+		// trace(namesSame);
+
+		var namesDifferent = name1 != name2;
+		// trace(namesDifferent);
+
+		var highscore = 10;
+		var newscore = 6
+
+		var scoreBeaten = newscore > highscore;
+		//trace(scoreBeaten);
+
+		newscore = 11;
+		scoreBeaten = newscore > highscore;
+		// trace(scoreBeaten);
+
+
+		var word = "interesting";
+
+		var wordIsLong = word.length >= 10;
+		//trace(wordIsLong);
+
+	}
+}
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This repository contains the code for my [Haxe Beginner's Tutorial Series](https://www.youtube.com/playlist?list=PLnweXVCg6yWodTlo3BQXicKJuGrTp_yeV).
 
-Each directory contains the code from the end of that video and the beginning of the next one.
+Each directory contains the code from the end of that video.
